96c7ef14e3758440bf2c379a11ada474c5c36014
Dinesh45R <dr3713846@gmail.com>
2024-03-02 12:22:24 +0530

    selvaowner

--- src/revision2ex/sample.java
+++ src/revision2ex/sample.java
@@ -4,12 +4,14 @@ import java.util.Scanner;
 
 public class sample {
  public static void main(String[] args) {
+	 System.out.println(233);
 	Scanner a=new Scanner(System.in);
 	System.out.println("Enter 1st number:");
 	System.out.println("Enter 2nd number:");
 	int a1=a.nextInt();
 	int a2=a.nextInt();
 	System.out.println(a1*a2);
+	System.out.println("hello");
 	
 }
 	
